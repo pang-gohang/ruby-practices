08220d79ae127b31189fd5d8532583afa66c8f47
pang-gohan <rasras3366@gmail.com>
2023-07-17 07:28:18 +0900

    .zero を使って判定するようにしました。

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,21 +1,11 @@
-def i_15?(i) #15の倍数か？
-    return i % 15 == 0
-end
-def i_3?(i) #3の倍数か？
-    return i % 3 == 0
-end
-def i_5?(i) #5の倍数か？
-    return i % 5 == 0
-end
-
 (1..20).each do |i|
-    if i_15?(i)
-        puts "FizzBuzz"
-    elsif i_3?(i)
-        puts "Fizz"
-    elsif i_5?(i)
-        puts "Buzz"
-    else
-        puts i
-    end
+  if (i % 15).zero?
+    puts "FizzBuzz"
+  elsif (i % 3).zero?
+    puts "Fizz"
+  elsif (i % 5).zero?
+    puts "Buzz"
+  else
+    puts i
+  end
 end

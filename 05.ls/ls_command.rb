0c9c44a02fdaec7ada734452a72ffcaf1600c34e
pang-gohan <ipad1117@gmail.com>
2023-08-25 06:24:56 +0900

    lsコマンド2の修正３回目です

--- 05.ls/ls_command.rb
+++ 05.ls/ls_command.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 def file_names
-  ls_option = ARGV.first
-  glob_option = ls_option&.include?('-a') ? File::FNM_DOTMATCH : nil
-  if glob_option.nil?
-    Dir.glob('*')
-  else
-    Dir.glob('*', glob_option)
-  end
+  ls_option = ARGV
+  glob_option = ls_option&.include?('-a') ? File::FNM_DOTMATCH : 0
+  Dir.glob('*', glob_option)
 end
 
 def ls_command

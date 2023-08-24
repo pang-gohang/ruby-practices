6fe3451fdef39a4d3bdfc4027a0ecbd51ba08eaa
pang-gohan <ipad1117@gmail.com>
2023-08-24 10:09:55 +0900

    ls2 修正2回目です。分岐をfile_namesメソッドに移動しました。

--- 05.ls/ls_command.rb
+++ 05.ls/ls_command.rb
@@ -1,24 +1,18 @@
 # frozen_string_literal: true
 
 def file_names
-  Dir.glob('*')
-end
-
-def file_names_a_option
-  Dir.glob('*', File::FNM_DOTMATCH)
+  ls_option = ARGV.first
+  glob_option = ls_option&.include?('-a') ? File::FNM_DOTMATCH : nil
+  if glob_option.nil?
+    Dir.glob('*')
+  else
+    Dir.glob('*', glob_option)
+  end
 end
 
 def ls_command
-  ls_option = ARGV.first
-  case ls_option
-  when nil
-    names = file_names
-  when '-a'
-    names = file_names_a_option
-  end
-
+  names = file_names
   max_name_length = names.map(&:length).max || 0
-
   num_columns = 3
   num_rows = (names.size + num_columns - 1) / num_columns
 

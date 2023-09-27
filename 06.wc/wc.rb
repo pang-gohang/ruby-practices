2d6f23ecb7c85277c7fb3847a135ed5f17e7b958
pang-gohan <ipad1117@gmail.com>
2023-09-27 14:42:01 +0900

    wcコマンド １回目訂正です。

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,14 +4,17 @@ require 'optparse'
 
 def main
   options = OptionParser.new.getopts(ARGV, 'lwc')
-  options.transform_values! { true } if options.values.all?(false)
-  args = ARGV
-  option_count = options.values.count(true)
+  options.transform_values! { true } if options.values.all?(false) # オプション未指定の場合
+  format_string = create_formatsgring(options)
 
-  if args.empty?
-    with_stdin($stdin.read, options, option_count)
-  else
-    with_file_path(args, options, option_count)
+  if ARGV.empty? # 標準入力の場合
+    output_line(calc_one_document($stdin.read, options), format_string)
+  elsif ARGV.size == 1 # ファイルが１つの場合
+    args = ARGV.join('')
+    content = File.read(args)
+    output_line(calc_one_document(content, options, args), format_string)
+  else # ファイルが複数の場合
+    output_many_file(ARGV, options, format_string)
   end
 end
 
@@ -27,57 +30,53 @@ def calc_bytesize(content)
   content.bytesize
 end
 
-def output_one_line_with_name(option_count, output, name)
-  case option_count
-  when 3
-    printf("%<lines>8d%<words>8d%<chars>8d %<filename>s\n", lines: output[0], words: output[1], chars: output[2], filename: name)
-  when 2
-    printf("%<lines>8d%<words>8d %<filename>s\n", lines: output[0], words: output[1], filename: name)
-  when 1
-    printf("%<lines>8d %<filename>s\n", lines: output[0], filename: name)
-  else
-    puts 'エラーです。'
-    exit
-  end
+def calc_one_document(string, options, filename = nil)
+  output = {}
+  output['l'] = count_lines(string) if options['l']
+  output['w'] = count_words(string) if options['w']
+  output['c'] = calc_bytesize(string) if options['c']
+  output['filename'] = filename if filename
+  output
 end
 
-def with_file_path(args, options, option_count)
-  total = [0, 0, 0]
+def output_line(document_infomation, format_string)
+  output = {
+    lines: document_infomation['l'] || nil,
+    words: document_infomation['w'] || nil,
+    chars: document_infomation['c'] || nil,
+    name: document_infomation['filename'] || nil
+  }
+  printf format(format_string, output)
+  puts
+  document_infomation
+end
+
+def create_formatsgring(options)
+  line_format = []
+  output_format = {
+    'l' => '%<lines>8d',
+    'w' => '%<words>8d',
+    'c' => '%<chars>8d',
+    'name' => ' %<name>s'
+  }
+
+  options.each do |key, value|
+    line_format.push(output_format[key]) if value
+  end
+  line_format.push(output_format['name'])
+  line_format.join('')
+end
+
+def output_many_file(args, options, format_string)
+  total = { 'l' => 0, 'w' => 0, 'c' => 0, 'filename' => 'total' }
   args.each do |arg|
     content = File.read(arg)
-    filename = arg
-    output = []
-    output.push(count_lines(content)) if options['l']
-    output.push(count_words(content)) if options['w']
-    output.push(calc_bytesize(content)) if options['c']
-    output.map.with_index do |count, index|
-      total[index] += count
-    end
-    output_one_line_with_name(option_count, output, filename)
+    subtotal = output_line(calc_one_document(content, options, arg), format_string)
+    total['l'] += subtotal['l'] || 0
+    total['w'] += subtotal['w'] || 0
+    total['c'] += subtotal['c'] || 0
   end
-  output_one_line_with_name(option_count, total, 'total') if args.size > 1
-end
-
-def with_stdin(content, options, option_count)
-  output = []
-  output.push(count_lines(content)) if options[:l]
-  output.push(count_words(content)) if options[:w]
-  output.push(calc_bytesize(content)) if options[:c]
-  output_one_line_without_name(option_count, output)
-end
-
-def output_one_line_without_name(option_count, output)
-  case option_count
-  when 3
-    printf("%<lines>8d%<words>8d%<chars>8d\n", lines: output[0], words: output[1], chars: output[2])
-  when 2
-    printf("%<lines>8d%<words>8d\n", lines: output[0], words: output[1])
-  when 1
-    printf("%<lines>8d\n", lines: output[0])
-  else
-    puts 'エラーです。'
-    exit
-  end
+  output_line(total, format_string)
 end
 
 main
